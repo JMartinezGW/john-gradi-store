9b8d4f8c160fdc7c3c70be86b2bdf279008ffe11
AlexanderM1927 <jhon71937@gmail.com>
2021-09-16 13:04:03 -0500

    fix add to cart

--- assets/section-productsgrid.js
+++ assets/section-productsgrid.js
@@ -1,10 +1,10 @@
 const myProductList = document.getElementsByClassName('myproduct__box')
-let productVariant = {}
+const productVariant = []
 for (let i = 0; i < myProductList.length; i++) {
     myProductList[i].addEventListener('mouseover', function (event) {
         const imagen = event.target.attributes.imagen.value // getting img from attibute html
         const productId = event.target.attributes.product.value
-        productVariant = event.target.attributes.variant
+        productVariant[productId] = event.target.attributes.variant
         document.getElementById('product-main-' + productId).src = imagen
     });
 }
@@ -12,13 +12,14 @@ for (let i = 0; i < myProductList.length; i++) {
     myProductList[i].addEventListener('click', function (event) {
         const imagen = event.target.attributes.imagen.value // getting img from attibute html
         const productId = event.target.attributes.product.value
-        productVariant = event.target.attributes.variant
+        productVariant[productId] = event.target.attributes.variant
         document.getElementById('product-main-' + productId).src = imagen
     });
 }
 const addToCartBtnList = document.getElementsByClassName('myproduct__btn')
 for (let i = 0; i < addToCartBtnList.length; i++) {
     addToCartBtnList[i].addEventListener('click', async function (event) {
+        const variantId = (productVariant[event.target.attributes.product.value] ? productVariant[event.target.attributes.product.value] : event.target.attributes.variant.value)
         const request = await fetch('/cart/add.js', {
             method: 'POST',
             headers: {
@@ -28,7 +29,7 @@ for (let i = 0; i < addToCartBtnList.length; i++) {
                 items: [
                     {
                         quantity: 1,
-                        id: event.target.attributes.product.value
+                        id: variantId
                     }
                 ]
             })
