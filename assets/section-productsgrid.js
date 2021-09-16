179be967514e43bc85fdf86a5732c7bcf0df7279
AlexanderM1927 <jhon71937@gmail.com>
2021-09-16 14:27:44 -0500

    testing

--- assets/section-productsgrid.js
+++ assets/section-productsgrid.js
@@ -22,7 +22,7 @@ for (let i = 0; i < addToCartBtnList.length; i++) {
     addToCartBtnList[i].addEventListener('click', async function (event) {
         const variantId = (productVariant[event.target.attributes.product.value] ? productVariant[event.target.attributes.product.value] : event.target.attributes.variant.value)
         try {
-            await fetch('/cart/add.js', {
+            const request = await fetch('/cart/add.js', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -36,6 +36,7 @@ for (let i = 0; i < addToCartBtnList.length; i++) {
                     ]
                 })
             });
+            console.log(request)
         } catch (error) {
             console.log('error')
             console.error(error)
