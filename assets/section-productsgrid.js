204d26a26d63606280a19299ae964df679165d65
AlexanderM1927 <jhon71937@gmail.com>
2021-09-16 14:18:52 -0500

    testing error when adding products without stock

--- assets/section-productsgrid.js
+++ assets/section-productsgrid.js
@@ -38,6 +38,7 @@ for (let i = 0; i < addToCartBtnList.length; i++) {
             });
         } catch (error) {
             console.log(error)
+            console.log(error.response)
         }
     });
 }
