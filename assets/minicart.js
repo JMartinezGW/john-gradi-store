42a6b6bd1fda11f835f7b9b83cb1668f8a758584
AlexanderM1927 <jhon71937@gmail.com>
2021-09-17 09:51:37 -0500

    testing minicart

--- assets/minicart.js
+++ assets/minicart.js
@@ -20,8 +20,35 @@ const showOverlay = () => {
 const getProductsMiniCart = async () => {
     try {
         const request = await fetch('/cart.js')
-        console.log(request)
+        if (request.status === 200) {
+            const items = request.response.json()
+            console.log(items)
+            // for (let i = 0; i < array.length; i++) {
+                
+            // }
+        }
     } catch (error) {
         console.error(error)
     }
+}
+
+const productProfile = (item) => {
+    const html = `
+    <div class="row">
+        <div class="col-3">
+            ${item.product_title}
+        </div>
+        <div class="col-4">
+            ${item.product_title}
+        </div>
+        <div class="col-2">
+            <input type="button" onclick="${item.quantity++}" value="-">
+            <input type="button" onclick="${item.quantity--}" value="+">
+        </div>
+        <div class="col-3">
+            ${(item.price * item.quantity)}
+        </div>
+    </div>
+    `
+    return html
 }
