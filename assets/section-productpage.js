e4065aa0ec5ffc330330352ee23ea866e09af2ff
AlexanderM1927 <jhon71937@gmail.com>
2021-09-17 14:52:52 -0500

    fix

--- assets/section-productpage.js
+++ assets/section-productpage.js
@@ -1,7 +1,7 @@
 let variantSelected = 0
 const variants = document.getElementsByName('variant')
 for (let i = 0; i < variants.length; i++) {
-    variants[i].addEventListener('mouseover', function (event) {
+    variants[i].addEventListener('click', function (event) {
         const image = event.target.dataset.image
         const variantId = event.target.dataset.variant
         variantSelected = variantId
