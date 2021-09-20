55fc15fa3dc8e730d749747c34d41050c42d4774
AlexanderM1927 <jhon71937@gmail.com>
2021-09-20 15:40:24 -0500

    testing slider

--- assets/section-productpage.js
+++ assets/section-productpage.js
@@ -42,3 +42,43 @@ document.getElementById('btn-add-cart').addEventListener('click', async () => {
         }
     }
 })
+
+// Slider
+
+const slider = document.getElementById('slider')
+let sliderSection = document.getElementsByClassName('slider__section')
+let sliderSectionLast = sliderSection[sliderSection.length - 1]
+
+const btnLeft = document.getElementById('btn-left')
+const btnRight = document.getElementById('btn-right')
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast)
+
+function next () {
+    const sliderSectionFirst = document.getElementsByClassName('slider__section')[0]
+    slider.style.marginLeft = '-200%'
+    slider.style.transition = 'all 0.5s'
+    setTimeout(() => {
+        slider.style.transition = 'none'
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst)
+        slider.style.marginLeft = '-100%'
+    }, 500)
+}
+
+function prev () {
+    let sliderSection = document.getElementsByClassName('slider__section')
+    let sliderSectionLast = sliderSection[sliderSection.length - 1]
+    slider.style.marginLeft = '0'
+    slider.style.transition = 'all 0.5s'
+    setTimeout(() => {
+        slider.style.transition = 'none'
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast)
+        slider.style.marginLeft = '-100%'
+    }, 500)
+}
+
+btnRight.addEventListener('click', next())
+
+btnLeft.addEventListener('click', prev())
+
+setInterval(next(), 5000)
