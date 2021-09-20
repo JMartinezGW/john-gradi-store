2d7409c447739d9d0ef43419abdae699ab27c7dd
AlexanderM1927 <jhon71937@gmail.com>
2021-09-20 16:25:36 -0500

    testing

--- assets/slider.js
+++ assets/slider.js
@@ -9,6 +9,16 @@ let sliderSectionLast = sliderSection[sliderSection.length - 1]
 
 let sliderActual = 0
 
+const getInfomationSlide = () => {
+    const info = document.getElementById('info')
+    info.innerHTML=`
+    <h2>${dataSlider[sliderActual].title}</h2>
+    ${dataSlider[sliderActual.description]}
+    `
+}
+
+getInfomationSlide()
+
 const btnLeft = document.getElementById('btn-left')
 const btnRight = document.getElementById('btn-right')
 slider.insertAdjacentElement('afterbegin', sliderSectionLast)
@@ -16,7 +26,7 @@ slider.insertAdjacentElement('afterbegin', sliderSectionLast)
 const nextSlide = () => {
     if (sliderActual < sliderSection.length - 1) sliderActual++
     else if(sliderActual === sliderSection.length - 1) sliderActual = 0
-    console.log(dataSlider[sliderActual])
+    getInfomationSlide()
     const sliderSectionFirst = document.getElementsByClassName('slider__section')[0]
     slider.style.marginLeft = '-200%'
     slider.style.transition = 'all 0.5s'
@@ -31,7 +41,7 @@ const prevSlide = () => {
     let sliderSection = document.getElementsByClassName('slider__section')
     if (sliderActual === 0) sliderActual = sliderSection.length - 1
     else sliderActual--
-    console.log(dataSlider[sliderActual])
+    getInfomationSlide()
     let sliderSectionLast = sliderSection[sliderSection.length - 1]
     slider.style.marginLeft = '0'
     slider.style.transition = 'all 0.5s'
